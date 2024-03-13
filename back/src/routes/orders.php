04686afbace1b3f511bc07a467a322800d560613
nunes-willi <nuneswillroberto@gmail.com>
2024-03-13 07:31:48 -0300

    orders

---
+++ back/src/routes/orders.php
@@ -0,0 +1,15 @@
+<?php
+// include "../service/home.php";
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: *");
+// header ('Access-Control-Allow-Method: GET, DELETE, POST');
+
+// switch ($_REQUEST['action']){
+//     case 'post';
+//     $total = $_POST['total'];
+//     $tax = $_POST['tax'];
+//     break;
+
+//     case 'get';
+    
+// }
